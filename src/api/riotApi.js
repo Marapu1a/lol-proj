66ec56d2d5fae4f8d65feec2f0f0d37de8c70a67
Valentin <marapulets87@yandex.ru>
2024-10-28 18:55:17 +0300

    Проверка с апи ключем и исправленным запросом

--- src/api/riotApi.js
+++ src/api/riotApi.js
@@ -4,7 +4,7 @@ const pref = '/api/rito/'
 
 // Функция для получения PUUID по gameName и tagLine
 export const getSummonerPUUID = (gameName, tagLine) => {
-    return axios.get(`${pref}riot/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`)
+    return axios.get(`${pref}riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`)
         .then(response => {
             console.log(response.data.puuid)
             return response.data;
@@ -17,7 +17,7 @@ export const getSummonerPUUID = (gameName, tagLine) => {
 
 // Функция для получения списка из 5 matchId
 export const getMatchHistory = (puuid) => {
-    return axios.get(`${pref}lol/lol/match/v5/matches/by-puuid/${puuid}/ids?count=5`) //вручную количество матчей изменил, чтоб избежать ограничения RIOT
+    return axios.get(`${pref}lol/match/v5/matches/by-puuid/${puuid}/ids?count=5`) //вручную количество матчей изменил, чтоб избежать ограничения RIOT
         .then(response => {
             return response.data;
         })
@@ -29,7 +29,7 @@ export const getMatchHistory = (puuid) => {
 
 // Функция для получения подробной статистики матча по matchId
 export const getMatchDetails = (matchId) => {
-    return axios.get(`${pref}lol/lol/match/v5/matches/${matchId}`)
+    return axios.get(`${pref}lol/match/v5/matches/${matchId}`)
         .then(response => {
             return response.data;
         })
